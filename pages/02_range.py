a75d10ffe86b0754a9b947d3f1f2248894471b86
Lwyi2929 <lwyi2929@gmail.com>
2025-11-24 16:55:01 +0000

    modified:   pages/02_range.py

--- pages/02_range.py
+++ pages/02_range.py
@@ -12,7 +12,7 @@ def create_map():
         sidebar_visible=True,
     )
     m.add_basemap("OpenStreetMap")
-    m.add_geojson("https://raw.githubusercontent.com/lwyi2929/20251119Solara_WebGIS_Demo/main/ma_river_wgs84.geojson", name="station")
+   # m.add_geojson("https://raw.githubusercontent.com/lwyi2929/20251119Solara_WebGIS_Demo/main/ma_river_wgs84.geojson", name="station")
     m.add_geojson("https://raw.githubusercontent.com/leoluyi/taipei_mrt/345dd492fa9c0138c126b3de75483a2881ed8991/routes.geojson", name="routes")
 
     return m
