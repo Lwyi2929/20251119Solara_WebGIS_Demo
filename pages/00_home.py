3af6c1a14ac65c03e2279ec7978d6e66ba01be12
Lwyi2929 <lwyi2929@gmail.com>
2025-11-24 14:46:21 +0000

    modified:   pages/00_home.py

--- pages/00_home.py
+++ pages/00_home.py
@@ -1,16 +1,9 @@
 import solara
+import pandas as pd
 
-# 1. 建立一個「響應式」變數 (元件的私有記憶)
-count = solara.reactive(0)
 
-# 2. 定義一個 Solara 元件 (用 @ 裝飾)
 @solara.component
 def Page():
-    solara.Title("我的 Solara App")
-    solara.Markdown(f"## 按鈕被點擊了 {count.value} 次！")
+    solara.Title("我的 Solara_WebGIS_Demo")
+    solara.Markdown("馬太鞍溪堰塞湖 事件於 2025 年 9 月 23 日 下午約 2 點 50 分 發生壩頂溢流，因豪雨帶來的暴漲溪水，造成馬太鞍溪河岸堤防出現破口，導致周邊低窪地區淹水。潰堤主要與短時間內大量降雨、上游集水區排水不及，以及堤防老化或結構受損等因素有關。事故發生後，相關單位進行緊急搶修與疏散，並加強後續堤防的安全檢查與整治。")
 
-    def increment():
-        count.value += 1 # 改變狀態值
-
-    # 3. 建立一個按鈕，綁定 on_click 事件
-    solara.Button("點我！", on_click=increment)
