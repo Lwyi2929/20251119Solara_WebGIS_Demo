154ffe68c448942438f1c45879b5dc0e774309c4
Lwyi2929 <lwyi2929@gmail.com>
2025-11-24 15:51:16 +0000

    modified:   pages/01_map.py

--- pages/01_map.py
+++ pages/01_map.py
@@ -1,5 +1,6 @@
 import solara
 import leafmap.leafmap as leafmap # 使用 ipyleaflet (重量級) 後端
+import json
 
 # 1. 定義「繁重」的地圖建立函式
 def create_map():
@@ -10,7 +11,11 @@ def create_map():
         layout_height="600px",
         basemap="OpenStreetMap" # 先設一個保證會成功的預設值
     )
-    return m
+
+    with open("data/ma_river.geojson") as f:
+        gj = json.load(f)
+    m.add_geojson(gj, name="river")
+
 
 @solara.component
 def Page():
@@ -23,3 +28,4 @@ def Page():
     # 並且使用 solara.display() 來顯示渲染 widget
     with solara.Column(style={"width": "100%", "height": "650px"}):
         solara.display(map_object)
+
